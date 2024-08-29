6ddca7623ee01fb5301f1a247c04fd4db666e08c
Raiyan Sharif <raiyan.sharif.1234@gmail.com>
2024-08-29 12:34:05 +0600

    subtraction method added

--- src/main/java/com/Assignment/Assignment/AssignmentApplication.java
+++ src/main/java/com/Assignment/Assignment/AssignmentApplication.java
@@ -24,4 +24,8 @@ public class AssignmentApplication {
 	public static int addition(int number1, int number2) {
 		return number1 + number2;
 	}
+
+	public static int subtraction(int number1, int number2) {
+		return number1 - number2;
+	}
 }
