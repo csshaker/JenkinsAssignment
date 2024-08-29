47be636333642daba4e13bc07a741532d31b5ade
Shaker Ahmed <shakerahmed063@gmail.com>
2024-08-29 10:08:52 +0600

    unit test code added

--- src/test/java/com/Assignment/Assignment/AssignmentApplicationTests.java
+++ src/test/java/com/Assignment/Assignment/AssignmentApplicationTests.java
@@ -3,11 +3,14 @@ package com.Assignment.Assignment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class AssignmentApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(10, 10);
 	}
 
 }
