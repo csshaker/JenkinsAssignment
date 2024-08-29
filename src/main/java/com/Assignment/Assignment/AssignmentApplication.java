250dbc93b316cd239f34980ef69330c04ab79302
Shaker Ahmed <shakerahmed063@gmail.com>
2024-08-29 09:51:33 +0600

    test() method added

--- src/main/java/com/Assignment/Assignment/AssignmentApplication.java
+++ src/main/java/com/Assignment/Assignment/AssignmentApplication.java
@@ -8,9 +8,15 @@ public class AssignmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentApplication.class, args);
+		printSomething("testing....");
+		test();
 	}
 
 	public static void printSomething(String text) {
 		System.out.println(text);
+	}
+
+	public static void test(){
+		System.out.println("test");
 	}
 }
